090b37060990f42e22bf982899affff6195dafe0
dramlian <jankovdamian@gmail.com>
2025-10-03 21:36:39 +0200

    Some small test refactoring

--- backend/ChoreMaster.Tests/TestFiles/ChoreMasterTests.cs
+++ backend/ChoreMaster.Tests/TestFiles/ChoreMasterTests.cs
@@ -1,15 +1,24 @@
 ﻿namespace ChoreMaster.Tests;
 
-
 public class ChoreMasterTests : IClassFixture<TestSetupFixture>, IAsyncLifetime
 {
+    private int _defaultChoreThreshold = 7;
+    private int _updatedChoreThreshold = 14;
+    private string _defaultChoreName = "Sample Chore";
+    private string _defaultUsername = "sample";
+    private string _defaultUserEmail = "sample@sample";
 
     private readonly IChoreManagementService _choreManagementService;
-
     private readonly IUserManagementService _userManagerService;
 
     public ChoreMasterTests(TestSetupFixture fixture)
     {
+        _defaultChoreThreshold = 7;
+        _updatedChoreThreshold = 14;
+        _defaultChoreName = "Sample Chore";
+        _defaultUsername = "sample";
+        _defaultUserEmail = "sample@sample";
+
         _choreManagementService = fixture.GetChoreManagementService() ?? throw new Exception("No ChoreManagementService was setup in the fixture");
         _userManagerService = fixture.GetUserManagementService() ?? throw new Exception("No UserManagementService was setup in the fixture");
     }
@@ -98,14 +107,14 @@ public class ChoreMasterTests : IClassFixture<TestSetupFixture>, IAsyncLifetime
         var updatedChore = await _choreManagementService.UpdateChoreAsync(chore!.Id, new ChoreUpdateDto
         {
             Name = "Updated Chore",
-            Threshold = 14,
+            Threshold = _updatedChoreThreshold,
             IsReassignedable = false
         });
 
         Assert.NotNull(updatedChore);
         Assert.Equal(chore.Id, updatedChore?.Id);
         Assert.Equal("Updated Chore", updatedChore?.Name);
-        Assert.Equal(14, updatedChore?.Threshold);
+        Assert.Equal(_updatedChoreThreshold, updatedChore?.Threshold);
         Assert.Equal(chore?.AssignedTo?.Id, updatedChore?.AssignedTo?.Id);
         Assert.False(updatedChore?.IsReassignedable);
         await _choreManagementService.DeleteChoreAsync(chore!.Id);
@@ -135,13 +144,9 @@ public class ChoreMasterTests : IClassFixture<TestSetupFixture>, IAsyncLifetime
     [Fact]
     public async Task CreateCompleteReassignableChore()
     {
-        var user1 = await _userManagerService.CreateUserAsync(new UserDto("user1", "user1@example.com"));
-        var user2 = await _userManagerService.CreateUserAsync(new UserDto("user2", "user2@example.com"));
-
-        var chore = await _choreManagementService.CreateChoreAsync(CreateSampleChoreDto(user1!));
-        Assert.NotNull(chore);
-        Assert.NotEqual(chore?.Id, 0);
-        Assert.NotEmpty(await _choreManagementService.GetAllChoresAsync());
+        var user1 = await CreateFetchAndAssertUserAsync("user1", "user1@example.com");
+        var user2 = await CreateFetchAndAssertUserAsync("user2", "user2@example.com");
+        var chore = await CreateFetchAndAssertChoreAsync(user1!);
 
         var completedChore = await _choreManagementService.CompleteChoreAsync(chore!.Id, user1!.Id, user2!.Id);
         Assert.NotNull(completedChore);
@@ -157,11 +162,11 @@ public class ChoreMasterTests : IClassFixture<TestSetupFixture>, IAsyncLifetime
         Assert.Empty(await _choreManagementService.GetAllChoresAsync());
     }
 
-    private async Task<User> CreateFetchAndAssertUserAsync()
+    private async Task<User> CreateFetchAndAssertUserAsync(string? name = null, string? email = null)
     {
-        var user = await _userManagerService.CreateUserAsync(CreateSampleUserDto());
+        var user = await _userManagerService.CreateUserAsync(CreateSampleUserDto(name, email));
         Assert.NotNull(user);
-        Assert.NotEqual(user?.Id, 0);
+        Assert.True(user?.Id > 0, "User ID should be greater than 0");
         Assert.NotEmpty(await _userManagerService.GetAllUsersAsync());
         return user!;
     }
@@ -170,19 +175,19 @@ public class ChoreMasterTests : IClassFixture<TestSetupFixture>, IAsyncLifetime
     {
         var chore = await _choreManagementService.CreateChoreAsync(CreateSampleChoreDto(user));
         Assert.NotNull(chore);
-        Assert.NotEqual(chore?.Id, 0);
+        Assert.True(chore?.Id > 0, "Chore ID should be greater than 0");
         Assert.NotEmpty(await _choreManagementService.GetAllChoresAsync());
         return chore!;
     }
 
     private ChoreDto CreateSampleChoreDto(User user)
     {
-        return new ChoreDto(new Chore("Sample Chore", 7, user, true));
+        return new ChoreDto(new Chore(_defaultChoreName, _defaultChoreThreshold, user, true));
     }
 
-    private UserDto CreateSampleUserDto()
+    private UserDto CreateSampleUserDto(string? name, string? email)
     {
-        return new UserDto("sample", "sample@sample");
+        return new UserDto(name ?? _defaultUsername, email ?? _defaultUserEmail);
     }
 }
 
